5a5024c78221f8da766146e36b134f5dff4688f6
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-04-25 10:40:15 +0200

    oops

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1,27 +1,5 @@
 <?php
 
-/*
- *
- *  _____            _               _____           
- * / ____|          (_)             |  __ \          
- *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
- *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
- *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
- * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
- *                         __/ |                    
- *                        |___/                     
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author GenisysPro
- * @link https://github.com/GenisysPro/GenisysPro
- *
- *
-*/
-
 namespace pocketmine;
 
 use pocketmine\block\Air;
@@ -3313,6 +3291,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 					if(!$canCraft or !$recipe->getResult()->Equals($result)){
 						$this->server->getLogger()->debug("Unmatched recipe " . $recipe->getId() . " from player " . $this->getName() . ": expected " . $recipe->getResult() . ", got " . $result . ", using: " . implode(", ", $ingredients));
 						$this->inventory->sendContents($this);//deepEquals
+						break;
 					}
 
 					$used = array_fill(0, $this->inventory->getSize(), 0);
